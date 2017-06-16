b5d2bb7e1aba6f6baabd65ea05fe3580413504b3
Ikbal Kazar <mikbalkazar@gmail.com>
2017-06-16 15:38:03 +0300

    use RNDeviceInfo for cross-platform version tracking

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,8 @@
 import { Platform } from 'react-native'
+import DeviceInfo from 'react-native-device-info';
 
 export const IS_DEV = true
-export const APP_VERSION = '1.0.6'
+export const APP_VERSION = DeviceInfo.getVersion();
 export const IS_IOS = Platform.OS == 'ios'
 export const IS_ANDROID = Platform.OS == 'android'
 export const IS_TEST_SUGGESTIONS = false
