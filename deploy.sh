0ce606912c72028607363eacd7c9a04f6289327d
Andriy Bogomazov <andrii.bogomazov@gmail.com>
2017-06-21 22:57:37 +0300

    deployment settings

--- deploy.sh
+++ deploy.sh
@@ -25,10 +25,10 @@ fi
 
 git_name="$(git config user.name)"
 slack_url="https://slack.com/api/chat.postMessage?token=xoxp-71345121281-71466681605-132637077411-5cc6a4f19311fcba3c0dcf3f04c288a2"
-full_slack_url="$slack_url&channel=deploy&text=$git_name deploying elliot-mobile:$BRANCH $COMMIT on CodePush-$ENV&pretty=1"
+full_slack_url="$slack_url&channel=deploy&text=$git_name deploying '$PLATFORM' of elliot-mobile:$BRANCH $COMMIT on CodePush-$ENV&pretty=1"
 # Need to convert spaces to %20's for slack
 converted_slack_url="${full_slack_url// /%20}"
 curl -s "$converted_slack_url"
 
 
-code-push release-react $APPNAME $PLATFORM --deploymentName $ENV --mandatory 
+code-push release-react $APPNAME $PLATFORM --deploymentName $ENV --mandatory
