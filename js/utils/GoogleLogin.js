7fe099438ae69fcab580415942365489ac58e24a
Andrew He <he.andrew.mail@gmail.com>
2017-06-30 13:09:00 -0700

    Update Google Web Client ID

--- js/utils/GoogleLogin.js
+++ js/utils/GoogleLogin.js
@@ -1,10 +1,13 @@
 import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
+import {IS_DEV} from "../settings";
 
 // TODO: move client ids to a configuration file
 GoogleSignin.configure({
    scopes: ["https://www.googleapis.com/auth/calendar"], // what API you want to access on behalf of the user, default is email and profile
    iosClientId: "245808737532-gpqc29f2ifmvqprmetmmtr60gaoq0cfg.apps.googleusercontent.com", // only for iOS
-   webClientId: "245808737532-ch1af9npbnnrova04gi752au3j74agia.apps.googleusercontent.com", // client ID of type WEB for your server (needed to verify user ID and offline access)
+   webClientId: IS_DEV ?
+     "245808737532-0hl28tp7jsfsqcgr4fvvlu0mmb08ecia.apps.googleusercontent.com": // client ID of type WEB for staging server (needed to verify user ID and offline access)
+     "245808737532-k2hpl4mmrov6257fc36rp74040r21hhc.apps.googleusercontent.com", // client ID of type WEB for prod server (needed to verify user ID and offline access)
    offlineAccess: true // if you want to access Google API on behalf of the user FROM YOUR SERVER
 })
 
