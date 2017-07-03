5daf9fcd2c9ab2a9927cdab5170c453b1efe623b
Ikbal Kazar <mikbalkazar@gmail.com>
2017-07-03 17:03:34 +0300

    move ios status bar from native to react

--- ios/Elliot/MainViewController.swift
+++ ios/Elliot/MainViewController.swift
@@ -19,11 +19,7 @@ class MainViewController: UIViewController {
         ReactFactory.rootViewController = self
         reactView = ReactFactory.shared.createView(name: "Elliot",
                                                    props: ["nativeIOS": ["accessToken": AuthorizationManager.shared.serverAuthToken!]])
-        reactView?.frame = CGRect(x: 0, y: 20, width: view.frame.width, height: view.frame.height - 20)
+        reactView?.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         view.addSubview(reactView!)
-        
-        let barView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 20))
-        barView.backgroundColor = UIColor.navigationAndTabBar()
-        view.addSubview(barView)
     }
 }
