d030bd7eb3f8217de80a534801559f910df6dec9
Ikbal Kazar <mikbalkazar@gmail.com>
2017-07-06 14:18:49 +0300

    ios native-module that exports build environment

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,8 @@
-import { Platform } from 'react-native'
+import { Platform, NativeModules } from 'react-native'
 import DeviceInfo from 'react-native-device-info';
 
-export const IS_DEV = true
+console.log(NativeModules.Config);
+export const IS_DEV = NativeModules.Config.buildEnvironment == 'DEBUG'
 export const APP_VERSION = DeviceInfo.getVersion();
 export const IS_IOS = Platform.OS == 'ios'
 export const IS_ANDROID = Platform.OS == 'android'
