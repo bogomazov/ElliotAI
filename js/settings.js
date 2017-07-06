0ee389cd61dd1b4406e11f9550db5c99b57a5a01
Ikbal Kazar <mikbalkazar@gmail.com>
2017-07-06 16:16:13 +0300

    fix conflicts, minor wording change

--- js/settings.js
+++ js/settings.js
@@ -1,16 +1,11 @@
-import { Platform, NativeModules } from 'react-native'
+import { Platform } from 'react-native'
 import DeviceInfo from 'react-native-device-info';
 import Config from './utils/ConfigModule'
 
-<<<<<<< HEAD
-const BUILD_ENV_STAGING = 'DEBUG'
+const BUILD_ENV_DEV = 'DEBUG'
 const BUILD_ENV_PROD = 'RELEASE'
 
-export const IS_DEV = Config.buildEnvironment == BUILD_ENV_STAGING
-=======
-console.log(NativeModules.Config);
-export const IS_DEV = NativeModules.Config.buildEnvironment == 'DEBUG'
->>>>>>> d030bd7eb3f8217de80a534801559f910df6dec9
+export const IS_DEV = Config.buildEnvironment == BUILD_ENV_DEV
 export const APP_VERSION = DeviceInfo.getVersion();
 export const IS_IOS = Platform.OS == 'ios'
 export const IS_ANDROID = Platform.OS == 'android'
