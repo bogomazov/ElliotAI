a174c9e3f51c4667bbaa425ff9889a04980a156e
Ikbal Kazar <mikbalkazar@gmail.com>
2017-07-10 14:30:20 +0300

    fix fb login screen layout

--- js/containers/Intro.js
+++ js/containers/Intro.js
@@ -2,73 +2,52 @@ import React from 'react'
 import {
   Text,
   View,
-  Image
+  Image,
+  Dimensions,
 } from 'react-native'
 import Swiper from 'react-native-swiper'
 import strings from '../res/values/strings'
 import s from '../res/values/styles'
 
+const VERTICAL_MARGIN = 200;
+const SWIPER_HEIGHT = Dimensions.get('window').height - VERTICAL_MARGIN;
 
-// export default class LoginScene extends Component {
-//   render() {
-//     let introTexts =
-//     let
-//     return ();
-//   }
-// }
-
-// images need to be statically included in RN
 export default () =>
-<View style={styles.wrapper}>
-  <Swiper activeDotColor={'#808080'} loop autoplay autoplayDirection>
-  {[[strings.login1, require('../res/images/login1.png')],
-    [strings.login2, require('../res/images/login2.png')],
-    [strings.login3, require('../res/images/login3.png')]].map((slide, i: number) =>
-    <View key={i} style={styles.slide}>
-      <View style={styles.textWrapper}>
-        <Text style={[styles.text, s.nuxGrayText]}>{slide[0]}</Text>
-      </View>
-      <Image
-        style={styles.image}
-        source={slide[1]}/>
-    </View>)}
-</Swiper>
-</View>
+  <Swiper
+    height={SWIPER_HEIGHT}
+    activeDotColor={'#808080'}
+    loop
+    autoplay
+    autoplayDirection
+    autoplayTimeout={4}>
+    {[[strings.login1, require('../res/images/login1.png')],
+      [strings.login2, require('../res/images/login2.png')],
+      [strings.login3, require('../res/images/login3.png')]].map((slide, i: number) =>
+        <View key={i} style={styles.slide}>
+          <Text style={[styles.text, s.nuxGrayText]}>{slide[0]}</Text>
+          <Image
+            style={styles.image}
+            source={slide[1]}
+            resizeMode="contain"
+          />
+        </View>
+      )
+    }
+  </Swiper>
 
 var styles = {
-  wrapper: {
-
-  },
   slide: {
-
     flex: 1,
-    // height: 300,
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
     alignItems: 'center',
-    // marginBottom: 30
-    paddingBottom: 60
-    // backgroundColor: '#9DD6EB'
+    marginBottom: 40,
   },
-
-  textWrapper: {
-    height: 40,
-    marginBottom: 20,
-  },
-
   text: {
-    color: '#000',
-    fontSize: 14,
+    flex: 1,
     textAlign: 'center',
-    width: 300,
-    fontFamily: 'OpenSans-Light',
-    marginBottom: 30,
+    paddingBottom: 20,
+    paddingHorizontal: 50,
   },
   image: {
-    width: 230,
-    height: 300
+    flex: 10,
   },
-  logoText: {
-    flex: 1
-  }
 }
